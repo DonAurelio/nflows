672d664d4274d89fe0cfbf09950ff2643416dab0
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2025-03-13 01:07:10 +0000

    Add validation to make commands, add backup when cleaning, rename tests

--- install/simgrid_install.sh
+++ install/simgrid_install.sh
@@ -11,7 +11,10 @@ sudo apt install -y \
     libnuma-dev \
     numactl \
     hwloc \
-    cmake
+    cmake \
+    zip \
+    unzip
+
 
 wget https://github.com/simgrid/simgrid/releases/download/v3.35/simgrid-3.35.tar.gz
 tar xf simgrid-3.35.tar.gz
