45ecdf678091dc8aae7fb48c97077963ab09e9d1
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2025-03-19 20:21:13 +0000

    Delete scripts and files to reorganize

--- scratch/workflow_instance.py
+++
@@ -1,5 +0,0 @@
-import pathlib
-from wfcommons import Instance
-input_instance = pathlib.Path('montage-chameleon-2mass-005d-001.json')
-instance = Instance(input_instance=input_instance)
-instance.write_dot(pathlib.Path('sample-montage-workflow.dot'))
