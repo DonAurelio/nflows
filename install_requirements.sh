dce08e2ff08b1e77de3e29aad28919cdd8a7d24d
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2025-07-28 22:49:51 +0000

    update

--- install_requirements.sh
+++ install_requirements.sh
@@ -3,6 +3,8 @@ apt update
 apt install -y \
     build-essential \
     nlohmann-json3-dev \
+    libevent-dev libeigen3-dev \
+    libboost-dev libboost-context-dev libboost-stacktrace-dev \
     graphviz graphviz-dev libgraphviz-dev \
     hwloc libhwloc-dev \
     numactl libnuma-dev \
